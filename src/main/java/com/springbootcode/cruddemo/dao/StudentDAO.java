d7184793a56dab210049a7e31057460d24cff88e
ShaguftaS <shaguftha.96@gmail.com>
2026-05-02 23:38:29 +0530

    Querying objects from JPA

--- src/main/java/com/springbootcode/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/springbootcode/cruddemo/dao/StudentDAO.java
@@ -1,11 +1,17 @@
 package com.springbootcode.cruddemo.dao;
 
 import com.springbootcode.cruddemo.entity.Student;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
 
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
 
 }
--- src/main/java/com/springbootcode/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/springbootcode/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.springbootcode.cruddemo.dao;
 
 import com.springbootcode.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -30,5 +33,14 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class,id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        //create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        //return query results
+        return theQuery.getResultList();
     }
 }
